9b32ee6474d4d4b42ce0dd215a299766b73c7d27
SHAMMAS <muhdshammas612@gmail.com>
2026-03-16 20:32:21 +0530

    Add migration to alter product model options and badge field
    - Set default ordering for products by created_at in descending order.
    - Update badge field to include choices for 'None', 'New', 'Sale', and 'Hot', with a default value of '' and allow blank entries.

--- products/models.py
+++ products/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=0)
     old_price = models.DecimalField(max_digits=10, decimal_places=2)
     new_price = models.DecimalField(max_digits=10, decimal_places=2)
-    badge = models.CharField(max_length=20, choices=BADGE_CHOICES, default='')
+    badge = models.CharField(max_length=20, choices=BADGE_CHOICES, default='',blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
--- products/views.py
+++ products/views.py
@@ -9,6 +9,7 @@ def product_list(request):
     qs = Product.objects.filter(is_active=True)
     paginator = Paginator(qs, 8)  
     page_obj = paginator.get_page(request.GET.get('page', 1))
+    
     context = {
         'all_categories': all_categories,
         'paginator': paginator,
@@ -23,7 +24,7 @@ def home_page(request):
         'categories': categories,
         'featured_products': featured_products,
     }
-    print(f"Featured Products: {featured_products[0].image if featured_products else 'None'}")  # Debugging statement to check the featured products queryset
+    # print(f"Featured Products: {featured_products[0].image if featured_products else 'None'}")  # Debugging statement to check the featured products queryset
     return render(request, 'home.html', context)
 
 def deals(request):
