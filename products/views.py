4d86eb2b665fe72d3417287e86ca26e311871797
SHAMMAS <muhdshammas612@gmail.com>
2026-03-17 11:17:22 +0530

    Add cart item count to product list and home page views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Category, Product
 from django.core.paginator import Paginator
+from cart.models import Cart
 
 # Create your views here.
 
@@ -9,20 +10,25 @@ def product_list(request):
     qs = Product.objects.filter(is_active=True)
     paginator = Paginator(qs, 8)  
     page_obj = paginator.get_page(request.GET.get('page', 1))
-    
+    cart_count = Cart.objects.filter(user=request.user).first().items.count() if request.user.is_authenticated else 0
+    cart_items = Cart.objects.filter(user=request.user).first().items.all() if request.user.is_authenticated else []
     context = {
         'all_categories': all_categories,
         'paginator': paginator,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'cart_count': cart_count,
+        'cart_items': cart_items,
     }
     return render(request, 'product_list.html', context)
 
 def home_page(request):
     categories = Category.objects.filter(is_active=True)
     featured_products = Product.objects.filter(is_active=True, is_featured=True)
+    cart_count = Cart.objects.filter(user=request.user).first().items.count() if request.user.is_authenticated else 0
     context = {
         'categories': categories,
         'featured_products': featured_products,
+        'cart_count': cart_count,
     }
     # print(f"Featured Products: {featured_products[0].image if featured_products else 'None'}")  # Debugging statement to check the featured products queryset
     return render(request, 'home.html', context)
