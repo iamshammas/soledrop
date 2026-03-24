327c5e456b6388f021938cab624b08473da6727a
SHAMMAS <muhdshammas612@gmail.com>
2026-03-24 15:07:38 +0530

    Refactor code structure for improved readability and maintainability

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    expiration_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.code
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Coupon
 
 # Create your views here.
 
@@ -12,3 +13,11 @@ def checkout(request):
 
     # If it's a GET request, simply render the checkout page
     return render(request, 'checkout.html')
+
+
+# def order_history(request):
+    # Fetch the user's order history from the database
+    # For example, you might want to retrieve Order objects associated with the user
+    # orders = Order.objects.filter(user=request.user)
+
+    # return render(request, 'order_list.html', {'orders': orders})
